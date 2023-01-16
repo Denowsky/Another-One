b37dcee4c7655a41ab9adae94b2f2fa0a38dc82f
Denowsky <den.san404@gmail.com>
2023-01-16 08:59:44 +0300

    Проверка на входящие числа в 28 задаче добавлена

--- Fifth_seminar/The_26th_one.py
+++ Fifth_seminar/The_26th_one.py
@@ -1,14 +1,16 @@
 # Задача 26:
 # Напишите программу, которая на вход принимает два числа A и B,
 # и возводит число А в целую степень B с помощью рекурсии.
+try:
+    a = int(input("Введите число a: "))
+    b = int(input("Введите число b: "))
 
-a = int(input("Введите число a: "))
-b = int(input("Введите число b: "))
-
-def MakePow(arg1,arg2):
-    if arg2==1:
-        return arg1
-    else:
-        return arg1*MakePow(arg1,arg2-1)
-    
-print(MakePow(a,b))
+    def MakePow(arg1,arg2):
+        if arg2==1:
+            return arg1
+        else:
+            return arg1*MakePow(arg1,arg2-1)
+        
+    print(MakePow(a,b))
+except:
+    print("Некорректный ввод")
