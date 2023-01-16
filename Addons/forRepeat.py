1a8e3268752dc384bc90a64d4e3032490a3475b9
Denowsky <den.san404@gmail.com>
2023-01-16 16:22:23 +0300

    Addons - дополнительные задачи, сделаны 101, 102. 103 не понята

--- Addons/forRepeat.py
+++ Addons/forRepeat.py
@@ -3,38 +3,37 @@
 # Пример: 
 # при d = 0.001, π = 3.141    0.1 ≤ d ≤ 0.00000000001
 import math,random
-# try:
-#     d = float(input("Введите точность d: "))
-#     if d<0.00000000001 or d>0.1:
-#         exit()
-#     else:
-#         count = 0
-#         print(d)
-#         while d<1:
-#             d=d*10
-#             count+=1
-#         p = round(math.pi,count)
-#         print(p)
-# except:
-#     print("Точность введена не точно")
+try:
+    d = float(input("Введите точность d: "))
+    if d<0.00000000001 or d>0.1:
+        exit()
+    else:
+        count = 0
+        print(d)
+        while d<1:
+            d=d*10
+            count+=1
+        p = round(math.pi,count)
+        print(p)
+except:
+    print("Точность введена не точно")
 
 # Задача 102 Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
-# try:
-#     n = int(input("Введите натуральное число N: "))
-#     if n<0:
-#         exit()
-#     count = 1
-#     result = 1
-#     while count<=n:
-#         if count<n:
-#             print(count, end="*")
-#         else:
-#             print(count, end="")
-#         result*=count
-#         count+=1
-#     print(f'={result}')
-# except:
-#     print("N не подходит условиям")
+try:
+    n = int(input("Введите натуральное число N: "))
+    if n<0:
+        exit()
+    list = []
+    m = 2
+    while n>1:
+        if n%m!=0:
+            m+=1
+        else:
+            list.append(m)
+            n/=m
+    print(list)
+except:
+    print("N не подходит условиям")
 
 # Задача 103 Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл file1.txt многочлен степени k.
 # Пример:  k=2 
@@ -43,21 +42,17 @@ import math,random
 # x*x + 5 = 0 
 # 10*x*x = 0
 
-data = open('file1.txt', 'w')
-def ListAppend(list, size):
-    for i in range(size):
-        list.append(random.randint(0, size))
-    
-k=int(input("k:"))
-# a=[]
-# ListAppend(a, 100)
-data.write(f'{(k)}*x*x+4*x+5=0')
+# data = open('file1.txt', 'w')
+# def ListAppend(list, size):
+#     for i in range(size):
+#         list.append(random.randint(0, size))
 
 
 # Задача 104. Даны два файла file1.txt и file2.txt, в каждом из которых находится запись многочлена (
     # полученные в результате работы программы из задачи 103). Необходимо сформировать файл file_sum.txt, содержащий сумму многочленов.
 
 # Задача 105 Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+text = 'Напишите программу, удаляющую из текста все слова, содержащие ""абв""'
 
 # Задача 106 Создайте программу для игры с конфетами человек против человека.
 # Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. 
