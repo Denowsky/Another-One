9046909867957ff50e2656f5a52a079e9fafe4be
denowsky <den.san404@gmail.com>
2023-01-26 00:13:25 +0300

    Сделана задача по 7-му семинару

---
+++ Seventh_seminar/functions.py
@@ -0,0 +1,88 @@
+from datetime import time
+import os
+from random import randint
+
+def clear_screen():
+    os.system('clear') # ноутбук с linux - я в командировке)
+
+def print_lines(dataprint):
+    while(True):
+        for line in dataprint:
+            print(",".join(line))
+        answer = input("Enter для возврата в меню:>")
+        if answer == "": return
+                      
+def print_drivers():
+    result = []
+    with open('crew.txt', 'r', encoding='utf8') as datafile:
+        for line in datafile:
+            line = line.strip('\n')
+            result.append(line.split(','))
+        return result
+    
+def add_driver():
+    count = 0
+    while(True):
+        with open('crew.txt', 'r+', encoding='utf8') as datafile:
+            answer = input("Enter - вернуться или Введите имя(англ): ")
+            if answer == "": return
+            else:
+                for line in datafile:
+                    count+=1
+                datafile.write(f'\ndriver_{count+1}, {answer}')
+
+def print_busses():
+    result = []
+    with open('vehicles.txt', 'r', encoding='utf8') as datafile:
+        for line in datafile:
+            line = line.strip('\n')
+            result.append(line.split(','))
+        return result
+    
+def add_bus():
+    count = 0
+    while(True):
+        with open('vehicles.txt', 'r+', encoding='utf8') as datafile:
+            answer = input("Enter - вернуться или Введите марку автобуса(англ): ")
+            number = input("Введите номер автобуса(прим. u999rt66): ")
+            if answer == "": return
+            else:
+                for line in datafile:
+                    count+=1
+                datafile.write(f'\nbus{count+1}, {number}, {answer}')
+                
+def print_routes(bus, driver):
+    result = []
+    with open('route.txt', 'r', encoding='utf8') as datafile:
+        for line in datafile:
+            line = line.strip('\n')
+            result.append(line.split(','))
+        for i in result:
+            for j in bus:
+                if i[2].strip(" ") == j[0]:
+                    i[2] = ",".join(j)
+            for k in driver:
+                if i[3].strip(" ") == k[0]:
+                    i[3] = ",".join(k)
+        return(result)
+    
+def show_route(route):
+     while(True):
+        for line in route:
+            print(f'номер маршрута: {line[0].strip(" ")}\nномер рейса: {line[1].strip(" ")}\nавтобус: {line[2].strip(" ")}\nводитель: {line[3].strip(" ")}\n')
+        answer = input("Enter для возврата в меню:>")
+        if answer == "": return   
+    
+def add_route():
+    count = 0
+    while(True):
+        with open('routes.txt', 'r+', encoding='utf8') as datafile:
+            answer = input("Enter - вернуться или Введите рейсовый номер: ")
+            number = input("Введите данные автобуса(Id): ")
+            driver = input("Введите данные водителя(Id): ")
+            if answer == "": return
+            else:
+                for line in datafile:
+                    count+=1
+                datafile.write(f'\nm{count+1}, {answer}, {number}, {driver}')
+                
--- Seventh_seminar/main.py
+++ Seventh_seminar/main.py
@@ -1,26 +1,28 @@
 
-import func2
+import functions as func
 
 if __name__ == '__main__':
-    menu = '''1 - Показать все записи\n2 - Найти запись по имени, фамилии, отчеству или телефону\n3 - Добавить новый контакт\n4 - Удалить контакт\n5 - Изменить номер телефона у контакта\n6 - Выход'''
+    menu = '''1 - Вывод водителей\n2 - Добавление водителя\n3 - Вывод автобусов\n4 - Добавление автобуса\n5 - Вывод маршрутов\n6 - Добавление маршрута\n7 - Выход'''
     while (True):
-        func2.clear_screen()
+        func.clear_screen()
         print(menu)
         answer = input('>:')
         match answer:
             case "1":
-                func2.print_lines(func2.show_lines())
+                func.print_lines(func.print_drivers())
             case "2":
-                func2.find_lines()          
+                func.add_driver()          
             case "3":
-                func2.add_lines()
+                func.print_lines(func.print_busses())
             case "4":
-                func2.remove_lines()
+                func.add_bus()
             case "5":
-                func2.change_lines()
+                func.show_route(func.print_routes(func.print_busses(), func.print_drivers()))
             case "6":
+                func.add_route()
+            case "7":
                 exit(0)
             case _:
                 print("неверный ввод")
-                func2.time.sleep(3)
+                func.time.sleep(3)
             
