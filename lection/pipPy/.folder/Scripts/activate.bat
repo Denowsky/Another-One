08eb7e7d7f054cd4c36a73b281089d30ef5933f1
Denowsky <den.san404@gmail.com>
2023-01-31 21:55:33 +0300

    Бот функционирует, в addons реализована игра в конфеты на одного игрока

--- lection/pipPy/.folder/Scripts/activate.bat
+++ lection/pipPy/.folder/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sanden\OneDrive - Inter IKEA Group\Documents\Кладовка Дениса\GIT\MyGit\Homework\lection\pipPy\.folder
+set VIRTUAL_ENV=C:\Users\Densa\OneDrive\Документы\Git\Homework\lection\pipPy\.folder
 
 if not defined PROMPT set PROMPT=$P$G
 
--- lection/pipPy/bot_commands.py
+++ lection/pipPy/bot_commands.py
@@ -1,9 +1,35 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import datetime
+from spy import log_bot
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def hello_bot(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    log_bot(update, context)
+    await update.message.reply_text(f'Oh hi, {update.effective_user.first_name}!')
     
+async def start_bot(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    log_bot(update, context)
+    await update.message.reply_text(f'Hello, is anybody here?')
     
+async def help_bot(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    log_bot(update, context)
+    await update.message.reply_text(f'My commands: \n/hello\n/help\n/sum\n/time')
+    
+async def sum_bot(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    log_bot(update, context)
+    msg = update.message.text
+    print(msg)
+    items = msg.split()
+    if len(items)>3 or len(items)<3:
+        await update.message.reply_text(f'Sorry, mate, it\'s seems wrong...\nGive me: /sum number1 number2\nPlease')
+    else: 
+        x = int(items[1])
+        y = int(items[2])
+        await update.message.reply_text(f'{x} + {y} = {x+y}')    
+
+async def time_bot(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    log_bot(update, context)
+    await update.message.reply_text(f'{datetime.datetime.now().time()}')    
+
 # https://core.telegram.org/api
 # https://python-telegram-bot.org/
